e2c58e8c274a7d8c7fa30cd261752d3e6cf0ef1a
Shannon Goddard <Uthingkithunk@aol.com>
2022-09-12 22:04:06 -0700

    error handling cell phone responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,6 +101,25 @@
   }
 
   /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
    * Scrool with ofset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
