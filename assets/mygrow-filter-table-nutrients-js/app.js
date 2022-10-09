18d01f16f99ddb6e94102f1f3f2251a76c66a513
Shannon Goddard <Uthingkithunk@aol.com>
2022-10-08 20:04:12 -0700

    working on table that will filter as user selects

---
+++ assets/mygrow-filter-table-nutrients-js/app.js
@@ -0,0 +1,48 @@
+function hide_show_table(col_name)
+{
+ var checkbox_val=document.getElementById(col_name).value;
+ if(checkbox_val=="show")
+ {
+  var all_col=document.getElementsByClassName(col_name);
+  for(var i=0;i<all_col.length;i++)
+  {
+   all_col[i].style.display="none";
+  }
+  document.getElementById(col_name+"_head").style.display="none";
+  document.getElementById(col_name).value="hide";
+ }
+	
+ else
+ {
+  var all_col=document.getElementsByClassName(col_name);
+  for(var i=0;i<all_col.length;i++)
+  {
+   all_col[i].style.display="table-cell";
+  }
+  document.getElementById(col_name+"_head").style.display="table-cell";
+  document.getElementById(col_name).value="show";
+  $("#taskButton").show();
+ }
+}
+
+//////////////////stores table after selection
+$(function(){
+  
+  // append this attribute to the element you want the html stored of.
+  $("#table1").attr("contenteditable", "true")
+
+  var content = document.getElementById('table1');
+
+  // save the page's state after user selects and clicks "select"
+  $("#taskButton").click(function() {
+    localStorage.setItem('page_html', JSON.stringify(content.innerHTML));
+  });
+
+  // retrieve local storage data
+  var arr = JSON.parse( localStorage.getItem('page_html') );
+  if (arr) {
+    content.innerHTML = arr;
+  }
+  console.log(arr);
+});
+
