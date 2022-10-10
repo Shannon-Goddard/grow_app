05e291f1117287faccb167d1e323789f6cf87e12
Shannon Goddard <Uthingkithunk@aol.com>
2022-10-10 16:15:09 -0700

    circular movement through mygrow

---
+++ assets/mygrow-js/app.js
@@ -0,0 +1,21 @@
+// retrieve local storage data
+/////Retrieve Plant Logo
+var plantLogo = localStorage.getItem('plantLogo');
+
+if (plantLogo) {
+  $("#mylogo").attr("src" , plantLogo);
+  document.getElementById("MyGrowText").style.display = "block";
+  document.getElementById("DeleteButton").style.display = "block";
+  document.getElementById("MyGrowAdd").style.display = "none";
+};
+  
+
+
+
+/////////////delete button
+//localStorage.clear();
+// //BUTTON CLEAR
+DeleteButton.addEventListener('click', function () {
+  localStorage.clear();
+  location.reload();
+});
