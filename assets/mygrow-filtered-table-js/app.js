5796de61d95b20d4fdc73fb053903f61084626db
Shannon Goddard <Uthingkithunk@aol.com>
2022-10-16 21:14:22 -0700

    troubleshooting table

--- assets/mygrow-filtered-table-js/app.js
+++ assets/mygrow-filtered-table-js/app.js
@@ -1,3 +1,23 @@
+//////////////////stores table after selection
+$(function(){
+  
+  // append this attribute to the element you want the html stored of.
+  $("#table1").attr("contenteditable", "true")
+
+  var content = document.getElementById('table1');
+
+  // save the page's state after user selects and clicks "select"
+  $(".get-started-btn").click(function() {
+    localStorage.setItem('page_html', JSON.stringify(content.innerHTML));
+  });
+
+  // retrieve local storage data
+  var arr = JSON.parse( localStorage.getItem('page_html') );
+  if (arr) {
+    content.innerHTML = arr;
+  }
+});
+
 /////Retrieve user selections
 function getValues() {
  ///////////////////////////////////////////////////////////////////
@@ -287,25 +307,7 @@ $(".twentyfourFlowering").hide();
 };
 window.onload = getValues;
 
-//////////////////stores table after selection
-$(function(){
-  
-  // append this attribute to the element you want the html stored of.
-  $("#table1").attr("contenteditable", "true")
 
-  var content = document.getElementById('table1');
-
-  // save the page's state after user selects and clicks "select"
-  $(".get-started-btn").click(function() {
-    localStorage.setItem('page_html', JSON.stringify(content.innerHTML));
-  });
-
-  // retrieve local storage data
-  var arr = JSON.parse( localStorage.getItem('page_html') );
-  if (arr) {
-    content.innerHTML = arr;
-  }
-});
  
 
 
