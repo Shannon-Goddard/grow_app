5e203245e8f64b36e33a639b8c3395a8ff367fcd
Shannon Goddard <Uthingkithunk@aol.com>
2022-10-26 10:15:00 -0700

    changing light data

--- assets/mygrow-light-watts-js/data.js
+++ assets/mygrow-light-watts-js/data.js
@@ -8,25 +8,25 @@ var data = [
   {
     index: "1",
     watts: "250W",
-    info: "A lot of cannabis growers think they need big grow lights to get the yields they’re looking for, and for some growers that may be true! But for growers looking for about an ounce a month, a 250W HPS may be the best grow light available. A 250W HPS doesn’t get even close to as hot as the bigger HPS grow lights like the 400W and 600W, but it still gets the improved yields, density and appearance of buds grown under an HPS.",
+    info: "You do not need big grow lights to get big yields. A 250W does not get as hot as the bigger grow lights like the 400W and 600W, but it still gets the improved yields, density and appearance as them.",
     pic: "../assets/mygrow-light-watts-images/watts.png"
   },
   {
     index: "2",
     watts: "400W",
-    info: "When you’re just starting to foray into the world of larger grow lights, a 400W HPS is an appropriate first light to try. It’s energy efficient, relatively cheap, and makes for large colas than stretch far down the plant. These light are more popular than the 250W HPS, but less than the 600W HPS, and far less popular than the current line of LEDs or LECs. Still, these lights are a workhorse and will pay for themselves quickly in any medium-sized indoor cannabis grow.",
+    info: "400W is a great light to try. It’s energy efficient, relatively cheap, and makes for large colas. These light are more popular than the 250W, but less than the 600W.",
     pic: "../assets/mygrow-light-watts-images/watts.png"
   },
   {
     index: "3",
     watts: "600W",
-    info: "The 600W HPS is one of the most powerful and efficient setups you can buy to grow your own cannabis at home. Although the technology behind it is archaic, there’s no denying the high-yielding, high-potency power of the might 600W HPS. Part of the true power of this light comes from the fact that it can be kept so close to plants (as close as 8-10″) while lights of similar wattage have to be kept much further away (18″+).",
+    info: "The 600W HPS is one of the most powerful and efficient setups you can buy to grow your own cannabis at home. There’s no denying the high-yielding, high-potency power of a 600W. You can keep it close to plants. While lights of larger wattage have to be kept much further away.",
     pic: "../assets/mygrow-light-watts-images/watts.png"
   },
   {
     index: "4",
     watts: "1000W",
-    info: "Although there is a larger size MH/HPS grow light available (1000W), at that size it starts going outside the scope of a “hobbyist” grower as far as ease and yields. In addition to needing a lot of extra cooling, a 1000W HPS grow light is less efficient compared to a 600W grow light (as far as how much light is put out for electricity used). I think most hobbyist growers would be happier with a 600W, or even two 600W lights, over a 1000W.",
+    info: "At this size it starts going outside the scope of a “hobbyist” grower as far as ease and yields. In addition to needing a lot of extra cooling, a 1000W grow light is less efficient compared to a 600W grow light (as far as how much light is put out for electricity used). Most hobbyist growers would be happier with a 600W, or even two 600W lights, over a 1000W.",
     pic: "../assets/mygrow-light-watts-images/watts.png"
   }
 ]; 
