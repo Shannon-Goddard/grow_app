e790c9c7e5dac2a793b87e8be9b602fa6aa2ecc5
Shannon Goddard <Uthingkithunk@aol.com>
2023-01-19 13:26:51 -0800

    trouble shooting table hidden on phone view

--- assets/mygrow-filter-table-nutrients-js/app.js
+++ assets/mygrow-filter-table-nutrients-js/app.js
@@ -1,3 +1,18 @@
+//////////////////stores table after selection
+$(function(){
+  // append this attribute to the element you want the html stored of.
+  $("#table1").attr("contenteditable", "true")
+  var content = document.getElementById('table1');
+  // save the page's state after user selects and clicks "select"
+  $(".get-started-btn").click(function() {
+    localStorage.setItem('page_html', JSON.stringify(content.innerHTML));
+  });
+  // retrieve local storage data
+  var arr = JSON.parse( localStorage.getItem('page_html') );
+  if (arr) {
+    content.innerHTML = arr;
+  }
+});
 /////////////check boxes//////////////
 function hide_show_table(col_name)
 {
@@ -25,18 +40,4 @@ function hide_show_table(col_name)
  }
  
 }
-//////////////////stores table after selection
-$(function(){
-  // append this attribute to the element you want the html stored of.
-  $("#table1").attr("contenteditable", "true")
-  var content = document.getElementById('table1');
-  // save the page's state after user selects and clicks "select"
-  $(".get-started-btn").click(function() {
-    localStorage.setItem('page_html', JSON.stringify(content.innerHTML));
-  });
-  // retrieve local storage data
-  var arr = JSON.parse( localStorage.getItem('page_html') );
-  if (arr) {
-    content.innerHTML = arr;
-  }
-});
+
