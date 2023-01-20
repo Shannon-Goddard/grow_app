87c4df207d7208711240ff8b06decaaa74b5c36b
Shannon Goddard <Uthingkithunk@aol.com>
2023-01-19 16:36:06 -0800

    going back to when website worked on mobil

--- assets/mydiary-filtered-table-js/app.js
+++ assets/mydiary-filtered-table-js/app.js
@@ -3,7 +3,10 @@ $(function(){
   // append this attribute to the element you want the html stored of.
   $("#table1").attr("contenteditable", "true")
   var content = document.getElementById('table1');
-
+  // save the page's state after user selects and clicks "select"
+  $(".get-started-btn").click(function() {
+    localStorage.setItem('page_html', JSON.stringify(content.innerHTML));
+  });
   // retrieve local storage data
   var arr = JSON.parse( localStorage.getItem('page_html') );
   if (arr) {
@@ -14,6 +17,5 @@ $(function(){
 $(function(){
   $('table tr:not(:first)').hide();
   $('.notes').show();
-  $(":input").hide();
 });
 //////$('#mytable tr:not(:first)')
