d543c83ba09b4fb9b7df62930570b3cf7e1979c9
Shannon Goddard <Uthingkithunk@aol.com>
2023-12-26 20:16:43 -0800

    added new nutes to other 3 grows and new table build

--- assets/mygrow-plant-size-js/data.js
+++ assets/mygrow-plant-size-js/data.js
@@ -2,25 +2,25 @@ var data = [
   {
     index: "0",
     height: "1-2 feet",
-    info: "Some people put their seedlings or clones right into the flowering stage if they want to harvest quickly though this makes for extremely small plants. Just because they are smaller in size does not mean they will yield low, it just means you will achieve more per square meter in a shorter amount of time than conventionally grown large plants. Growing small plants can work perfectly for growers who are simply limited to grow space. It’s also important to remember that container size makes a big difference. Small containers constrain the roots and keep plants from getting as big as they could. For every foot tall you want your plant to be, the container should be eqal in gallon; 1 foot tall plant would need 1 gallon container.",
+    info: "Use a 1-2 gallon container",
     logo: "../assets/mygrow-plant-size-images/1-2foot.png"
   },
   {
     index: "1",
     height: "3-4 feet",
-    info: "The option to grow more varieties is possible when growing mid-sized plants. It is much easier to include more varieties in a large plant count than when limited to large plants. 3-gallon pots tend to produce mid-size plants",
+    info: "Use a 3 gallon container",
     logo: "../assets/mygrow-plant-size-images/3-4foot.png"
   },
   {
     index: "2",
     height: "5+ feet",
-    info: "A main advantage associated with larger-sized plants is larger-sized buds and plenty of them. Spending the time to nurture a large-sized Cannabis plant that will have had a long vegging time and an equally long flowering period can require a lot of hands-on maintenance, but the rewards are worth their weight in gold. Big roots means big shoots, which means that larger-sized plants should have a 5 gallon container.",
+    info: "Use a 5 gallon container",
     logo: "../assets/mygrow-plant-size-images/5-6foot.png"
   },
   {
     index: "3",
     height: "7+ feet",
-    info: "Due to the space needed from the ceiling to the exhaust, the exhaust from the light, and the light from your plant... Your cannabis plants WILL outgrow your grow space during the flowering stage! Unless you plan on using High-Stress Training (HST). I would recommend growing outside. Many growers begin germinating seeds as early as February and March in order to have big plants come harvest. You want to plant your plants in the ground outside, no later than, early to mid-May",
+    info: "Use a 7 gallon container",
     logo: "../assets/mygrow-plant-size-images/12foot.png"
   }
 ]; 
