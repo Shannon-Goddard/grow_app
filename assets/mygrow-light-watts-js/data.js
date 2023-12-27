68dcadff4287bf45ee0b6fd7e49ddffce4da410a
Shannon Goddard <Uthingkithunk@aol.com>
2023-12-26 19:37:59 -0800

    adding watt images and trouble shooting mobil view

--- assets/mygrow-light-watts-js/data.js
+++ assets/mygrow-light-watts-js/data.js
@@ -3,30 +3,30 @@ var data = [
     index: "0",
     watts: "125W",
     info: "With LED grow lights, you will generally get better results with many smaller LED panels, as opposed to a few big LED panels. For example, you will often get better results with 2 x 125W LEDs (total 250W) than you would with a single LED model that produces 250W. This is because more panels often make it easier for you to spread the light to where it’s needed.",
-    pic: "../assets/mygrow-light-watts-images/watts.png"
+    pic: "../assets/mygrow-light-watts-images/130w.png"
   },
   {
     index: "1",
     watts: "250W",
     info: "You do not need big grow lights to get big yields. A 250W does not get as hot as the bigger grow lights like the 400W and 600W, but it still gets the improved yields, density and appearance as them.",
-    pic: "../assets/mygrow-light-watts-images/watts.png"
+    pic: "../assets/mygrow-light-watts-images/240w.png"
   },
   {
     index: "2",
     watts: "400W",
     info: "400W is a great light to try. It’s energy efficient, relatively cheap, and makes for large colas. These light are more popular than the 250W, but less than the 600W.",
-    pic: "../assets/mygrow-light-watts-images/watts.png"
+    pic: "../assets/mygrow-light-watts-images/500w.png"
   },
   {
     index: "3",
     watts: "600W",
     info: "The 600W HPS is one of the most powerful and efficient setups you can buy to grow your own cannabis at home. There’s no denying the high-yielding, high-potency power of a 600W. You can keep it close to plants. While lights of larger wattage have to be kept much further away.",
-    pic: "../assets/mygrow-light-watts-images/watts.png"
+    pic: "../assets/mygrow-light-watts-images/730w.png"
   },
   {
     index: "4",
     watts: "1000W",
     info: "At this size it starts going outside the scope of a “hobbyist” grower as far as ease and yields. In addition to needing a lot of extra cooling, a 1000W grow light is less efficient compared to a 600W grow light (as far as how much light is put out for electricity used). Most hobbyist growers would be happier with a 600W, or even two 600W lights, over a 1000W.",
-    pic: "../assets/mygrow-light-watts-images/watts.png"
+    pic: "../assets/mygrow-light-watts-images/1000w.png"
   }
 ]; 
